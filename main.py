7b93b4dd981d826288c94cfdfdd72d1e21f49e49
ItsMikeyy <michaelcarey14@outlook.com>
2025-07-14 12:32:44 -0600

    allow for pptx

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import StreamingResponse
+from fastapi import FastAPI, File, HTTPException, UploadFile
+from fastapi.responses import JSONResponse, StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
 from pyt2s.services import stream_elements
+from pptx import Presentation
 import os
 import io
 import string
@@ -12,12 +13,12 @@ import tempfile
 app = FastAPI()
 
 origins = [
-    "https://pdf2tts-front.vercel.app",  #
+    "https://pdf2tts-front.vercel.app",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  
+    allow_origins=origins, 
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"],  
@@ -29,19 +30,65 @@ def root():
 
 @app.post("/convert")
 async def convert(file: UploadFile = File(...)):
-    if file.content_type != 'application/pdf':
-        return {"error": "File must be a PDF"}
+    if not (file.filename.endswith(".pptx") or file.filename.endswith(".pdf")):
+        raise HTTPException(status_code=400, detail="File must be a .pptx PowerPoint file or a .pdf file.")
 
+    print(f"Received file: {file.filename}")
+
+    contents = await file.read()
+    if file.filename.endswith(".pdf"):
+        return await parse_pdf(contents)
+    elif file.filename.endswith(".pptx"):
+        return await parse_pptx(contents)
+
+
+async def parse_pdf(contents):
     try:
         with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as temp_pdf:
             pdf_path = temp_pdf.name
-            contents = await file.read()
             temp_pdf.write(contents)
 
         extracted_text = extract_text(pdf_path)
-
+        print(f"Extracted text: {extracted_text[:100]}...")
         if len(extracted_text.strip()) == 0:
-            return {"error": "No extractable text found in the PDF."}
+            return JSONResponse(content={"error": "No extractable text found in the PDF."})
+
+        data = stream_elements.requestTTS(extracted_text, stream_elements.Voice.Amy.value)
+
+        return StreamingResponse(
+            io.BytesIO(data),
+            media_type="audio/mpeg",
+            headers={"Content-Disposition": "inline; filename=output.mp3"}
+        )
+
+    except Exception as e:
+        print(f"Error processing PDF: {str(e)}")
+        return JSONResponse(content={"error": f"An error occurred while processing the PDF: {str(e)}"})
+
+    finally:
+        if os.path.exists(pdf_path):
+            os.remove(pdf_path)
+
+def extract_pptx_text(pptx_path):
+    text = ""
+    ppt = Presentation(pptx_path)
+    for slide in ppt.slides:
+        for shape in slide.shapes:
+            if hasattr(shape, "has_text_frame") and shape.has_text_frame:
+                text += clean_text(shape.text_frame.text) + "\n"
+    return text
+
+async def parse_pptx(contents):
+    try:
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".pptx") as temp_pptx:
+            pptx_path = temp_pptx.name
+            temp_pptx.write(contents)
+
+        extracted_text = extract_pptx_text(pptx_path)
+        print(f"Extracted text: {extracted_text[:100]}...")
+        
+        if len(extracted_text.strip()) == 0:
+            return JSONResponse(content={"error": "No extractable text found in the PowerPoint."})
 
         data = stream_elements.requestTTS(extracted_text, stream_elements.Voice.Amy.value)
 
@@ -51,17 +98,12 @@ async def convert(file: UploadFile = File(...)):
             headers={"Content-Disposition": "inline; filename=output.mp3"}
         )
     except Exception as e:
-        return {"error": f"An error occurred while processing the PDF: {str(e)}"}
+        print(f"Error processing PPTX: {str(e)}")
+        return JSONResponse(content={"error": f"An error occurred while processing the PPTX: {str(e)}"})
 
     finally:
-        if os.path.exists(pdf_path):
-            os.remove(pdf_path)
-    
-    
-     
-
-        
-
+        if os.path.exists(pptx_path):
+            os.remove(pptx_path)
 
 def clean_text(text):
     allowed = string.ascii_letters + string.digits + string.punctuation + " \n"
